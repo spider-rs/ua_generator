f66659d5a5653cf9a76d8c6dc9b7f0841cc297b7
Mike Cronce <floppie@quadra-tec.net>
2022-12-30 17:08:35 -0500

    Add ua_list::STATIC_AGENTS and use that in ua::spoof_ua() (#3)
    * ua_generator/src/ua_list.rs:  Added constant STATIC_AGENTS so we can generate without allocating
    
    * ua_generator/src/ua.rs:  spoof_ua():  Use ua_list::STATIC_AGENTS instead of ua_list::agents
    
    * ua_generator/src/ua_list.rs:  Split agents out to one per line

--- ua_generator/src/ua.rs
+++ ua_generator/src/ua.rs
@@ -1,9 +1,9 @@
-pub use crate::ua_list::agents;
+pub use crate::ua_list::STATIC_AGENTS;
 use fastrand;
 
 /// Get a random UA from the most popular list pre-compiled generated from <https://apilayer.com/marketplace/user_agent-api#documentation-tab>.
 pub fn spoof_ua() -> &'static str {
-    let agent_list = agents(); // list of user agents to choose from.
+    let agent_list = STATIC_AGENTS; // list of user agents to choose from.
     let i = fastrand::usize(..agent_list.len());
 
     agent_list[i]
