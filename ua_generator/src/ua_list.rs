300471be1b65cdf596c4a2767e65396f91ab6538
j-mendez <j-mendez@users.noreply.github.com>
2023-10-08 00:40:15 +0000

    chore(agents): update agent list to latest

--- ua_generator/src/ua_list.rs
+++ ua_generator/src/ua_list.rs
@@ -1,14 +1,14 @@
 /// static list of agents pre-built
 pub const STATIC_AGENTS: &'static [&'static str; 9] = &[
-    "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; Win64; x64; Trident/6.0)",
-    "Mozilla/5.0 (Windows NT 6.2; Win64; x64; rv:49.0) Gecko/20100101 Firefox/49.0",
-    "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2939.59 Safari/537.36",
-    "Mozilla/5.0 (Android 5.1; Tablet; rv:51.0) Gecko/51.0 Firefox/51.0",
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:45.0) Gecko/20100101 Firefox/45.0",
-    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2760.12 Safari/537.36",
-    "Mozilla/5.0 (Android 4.4; Tablet; rv:49.0) Gecko/49.0 Firefox/49.0",
-    "Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:51.0) Gecko/20100101 Firefox/51.0",
-    "Mozilla/5.0 (X11; Ubuntu; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2859.22 Safari/537.36"
+    "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.3; Trident/6.0)",
+    "Mozilla/5.0 (Windows NT 6.1; rv:45.0) Gecko/20100101 Firefox/45.0",
+    "Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2738.32 Safari/537.36",
+    "Mozilla/5.0 (Android 4.4.3; Tablet; rv:47.0) Gecko/47.0 Firefox/47.0",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:50.0) Gecko/20100101 Firefox/50.0",
+    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2672.39 Safari/537.36",
+    "Mozilla/5.0 (Android 4.4.1; Tablet; rv:46.0) Gecko/46.0 Firefox/46.0",
+    "Mozilla/5.0 (X11; Linux i686; rv:51.0) Gecko/20100101 Firefox/51.0",
+    "Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2877.30 Safari/537.36"
 ];  
 
 /// user agent list
